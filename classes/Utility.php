b9b5a554b26dfff0241f1c12f9269946408af743
Nicole Swan <nicole.swan@bellevuecollege.edu>
2016-08-26 13:44:44 -0700

    Comment out debug lines

--- classes/Utility.php
+++ classes/Utility.php
@@ -7,12 +7,12 @@ class Utility {
             if ( mb_detect_encoding($value) == "UTF-8" ) {
                 //then encode it for UTF-16
                 $encoded_val = iconv("UTF-8", "UTF-16", $value);
-                echo "Key " . $key . " has UTF-8 encoding";
+                //echo "Key " . $key . " has UTF-8 encoding";
                 $_input_array[$key] = $encoded_val;
             } else if ( mb_detect_encoding($value) == "ASCII" && preg_match("/\r\n|\r|\n/", $value) ) {
                 //has newline characters so convert encoding
                 $encoded_val = iconv("ASCII", "UTF-16", $value);
-                echo "Key " . $key . " has ASCII encoding and new lines";
+                //echo "Key " . $key . " has ASCII encoding and new lines";
                 $_input_array[$key] = $encoded_val;
             }
             
