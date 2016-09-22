f47b88bef5158d2beb71661850646bcbead8ade8
Nicole Swan <nicole.swan@bellevuecollege.edu>
2016-09-21 17:38:59 -0700

    Update sample configuration file
    - Forgot to add new payment status global var to sample config - now
      added

--- config-sample.php
+++ config-sample.php
@@ -8,6 +8,9 @@ define ('DATABASE_PASSWORD', '');
 /* Set models available to which forms can be attached */
 define ('SQLSERVER_MODELS', 'AppliedAccounting_BAS,DataAnalytics_BAS,HealthcareInformatics_BAS,HealthcareManagement_BAS,HealthcarePromotion_BAS,InteriorDesign_BA,IST_BAS,MolecularBiosciences_BAS,Nursing_RN_BSN,RadiationImaging_BAS');
 
+/* Set default authorized payment status */
+define ('DEFAULT_PAYMENT_STATUS', '');
+
 /* Set nonce values */
 define ('NONCE_ACTION', '' );
 define ('NONCE_FIELD', '' );
